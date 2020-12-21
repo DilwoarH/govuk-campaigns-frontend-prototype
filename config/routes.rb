e46f6de74cfd9b80add7fb8e78252da810128b88
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-12-21 11:06:54 +0000

    adds campaigns route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "campaigns#index"
+  get "/campaigns", to: "campaigns#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
