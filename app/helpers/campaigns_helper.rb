63ea3935d9401767ae3b96db88826939c62c2636
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-12-21 15:09:03 +0000

    add some blocks

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,7 @@
 module CampaignsHelper
+  def render_govspeak(content)
+    render "govuk_publishing_components/components/govspeak" do
+      raw(Govspeak::Document.new(content, sanitize: false).to_html)
+    end
+  end
 end
